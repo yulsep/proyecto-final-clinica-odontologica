55f7b6e39c2eff99612f455941f0618af1901954
Yuliana Sepúlveda <yuliana.sepulveda@alternova.com>
2024-06-10 10:28:46 -0500

    first commit

---
+++ src/main/java/dh/backend/proyecto_final_clinica_odontologica/ProyectoFinalClinicaOdontologicaApplication.java
@@ -0,0 +1,13 @@
+package dh.backend.proyecto_final_clinica_odontologica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalClinicaOdontologicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalClinicaOdontologicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dh/backend/proyecto_final_clinica_odontologica/ProyectoFinalClinicaOdontologicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dh.backend.proyecto_final_clinica_odontologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalClinicaOdontologicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
