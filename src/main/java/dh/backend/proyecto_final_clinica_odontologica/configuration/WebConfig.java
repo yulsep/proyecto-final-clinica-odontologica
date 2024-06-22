c9d5314a5ccbe001297279aeb0381cb949648dd6
devArm <alopeze@unsa.edu.pe>
2024-06-21 20:56:03 -0500

    (fix) allow origin frontend localhost:5173

--- src/main/java/dh/backend/proyecto_final_clinica_odontologica/configuration/WebConfig.java
+++ src/main/java/dh/backend/proyecto_final_clinica_odontologica/configuration/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500/")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
